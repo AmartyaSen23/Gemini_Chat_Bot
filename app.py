c2d5914de1379f230c730ed5ada48bdc5808db6e
Amartya Sen <amartya.of.sen.is.18@gmail.com>
2026-07-03 20:15:18 +0530

    Update

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ except KeyError:
 
 # Configure Gemini
 genai.configure(api_key=api_key)
-model = genai.GenerativeModel('gemini-1.5-flash')
+model = genai.GenerativeModel('gemini-1.5-flash-latest')
 
 repo_input = st.text_input("Target GitHub Repository (Format: owner/repo)")
 
